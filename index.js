722cc43f008a45c84ebd4cac29bb9a50be7aed63
rockts <rockts@sina.com>
2018-05-15 00:08:40 +0800

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const { loadCollection, db } = require('./src/db')
+const { upload } = require('./src/upload')
+
+const app = express()
+const router = express.Router()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.get('/', (request, response) => {
+  response.send({
+    message: '你好 ~'
+  })
+})
+
+app.post('/', (request, response) => {
+  console.log(request.body)
+})
+
+router.route('/uploads')
+  .post(upload.array('uploads', 9), async (request, response, next) => {
+    const collection = await loadCollection('uploads', db)
+    const result = collection.insert(request.files)
+    db.saveDatabase()
+    response.send(
+      result
+    )
+  })
+
+app.use('/api', router)
+
+app.listen(8080, () => {
+  console.log('localhost:8080')
+})
